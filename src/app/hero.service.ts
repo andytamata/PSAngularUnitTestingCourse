a7d9114eb11736af9448153129275004a38acf85
gitName <a@b.c>
2023-01-09 12:06:09 +0000

    pipe tests

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -24,7 +24,7 @@ export class HeroService {
   getHeroes (): Observable<Hero[]> {
     return this.http.get<Hero[]>(this.heroesUrl)
       .pipe(
-        tap(heroes => this.log(`fetched heroes`)),
+        tap(() => this.log(`fetched heroes`)),
         catchError(this.handleError('getHeroes', []))
       );
   }
