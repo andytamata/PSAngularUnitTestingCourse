41cfdbc77c64112458a68d0023973bf3d3fd4eef
gitName <a@b.c>
2023-01-09 14:56:05 +0000

    service tests

---
+++ src/app/message.service.spec.ts
@@ -0,0 +1,42 @@
+import { MessageService } from "./message.service"
+
+describe('Message service' , () => {
+    let service: MessageService;
+
+    beforeEach(() => {
+        
+    })
+
+    it('should have no messages to start', () => {
+        // arrange
+        service = new MessageService();
+
+        // assert
+        expect(service.messages.length).toBe(0);
+    })
+
+    it('should add a message when add is called', () => {
+        // arrange
+        service = new MessageService();
+        
+        // act
+        service.add('message1');
+
+        // assert
+        expect(service.messages.length).toBe(1);
+    })
+
+    it('should remove all messages when clear is called', () => {
+        // arrange
+        service = new MessageService();
+        service.add('message1');
+        
+        // act
+        service.clear();
+
+        // assert
+        expect(service.messages.length).toBe(0);
+    })
+
+    
+})
